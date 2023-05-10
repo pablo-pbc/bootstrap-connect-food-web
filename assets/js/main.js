32b7dd710714a9420f24c4cb4a45b4cf1cd55ba2
pablo.carvalho <pablo.carvalho@icommgroup.com.br>
2023-05-10 17:22:06 -0300

    first commit Connect Food Web with JS HTML CSS BOOSTRAP.CSS ANIMATE.CSS and more

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+(function () {
+    //===== Prealoder
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+    /*=====================================
+    Nav bar function Sticky
+    ======================================= */
+    window.onscroll = function () {
+        let header_navbar = document.querySelector(".navbar-area");
+        let sticky = header_navbar.offsetTop;
+        let logo = document.querySelector('.navbar-brand img')
+
+        if (window.pageYOffset > sticky) {
+          header_navbar.classList.add("sticky");
+          logo.src = 'assets/images/logo/logo.svg';
+        } else {
+          header_navbar.classList.remove("sticky");
+          logo.src = 'assets/images/logo/white-logo.svg';
+        }
+
+        // show or hide the back-top-top button
+        let backToTo = document.querySelector(".scroll-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+    };
+    
+    // section menu active
+	function onScroll(event) {
+		let sections = document.querySelectorAll('.page-scroll');
+		let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+		for (let i = 0; i < sections.length; i++) {
+			let currLink = sections[i];
+			let val = currLink.getAttribute('href');
+			let refElement = document.querySelector(val);
+			let scrollTopMinus = scrollPos + 73;
+			if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
+				document.querySelector('.page-scroll').classList.remove('active');
+				currLink.classList.add('active');
+			} else {
+				currLink.classList.remove('active');
+			}
+		}
+	};
+
+    window.document.addEventListener('scroll', onScroll);
+    
+    // for menu scroll 
+    let pageLink = document.querySelectorAll('.page-scroll');
+
+    pageLink.forEach(elem => {
+        elem.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                offsetTop: 1 - 60,
+            });
+        });
+    });
+
+    // WOW active
+    new WOW().init();
+
+    //===== mobile-menu-btn
+    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    navbarToggler.addEventListener('click', function () {
+        navbarToggler.classList.toggle("active");
+    });
+})();
