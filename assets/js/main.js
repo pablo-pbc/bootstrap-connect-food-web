5f6bf976df76568c48814d535b83049d6ad1d8b1
pablo.carvalho <pablo.carvalho@icommgroup.com.br>
2023-05-10 21:12:49 -0300

    feature: form e-mail validation and senting email with static form

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,9 @@
     Nav bar function Sticky
     ======================================= */
     window.onscroll = function () {
-        let header_navbar = document.querySelector(".navbar-area");
-        let sticky = header_navbar.offsetTop;
-        let logo = document.querySelector('.navbar-brand img')
+        const header_navbar = document.querySelector(".navbar-area");
+        const logo = document.querySelector('.navbar-brand img')
+        let sticky = header_navbar.offsetTop;        
 
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
@@ -26,7 +26,7 @@
         }
 
         // show or hide the back-top-top button
-        let backToTo = document.querySelector(".scroll-top");
+        const backToTo = document.querySelector(".scroll-top");
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
             backToTo.style.display = "flex";
         } else {
@@ -36,7 +36,7 @@
     
     // section menu active
 	function onScroll(event) {
-		let sections = document.querySelectorAll('.page-scroll');
+		const sections = document.querySelectorAll('.page-scroll');
 		let scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
 		for (let i = 0; i < sections.length; i++) {
@@ -56,7 +56,7 @@
     window.document.addEventListener('scroll', onScroll);
     
     // for menu scroll 
-    let pageLink = document.querySelectorAll('.page-scroll');
+    const pageLink = document.querySelectorAll('.page-scroll');
 
     pageLink.forEach(elem => {
         elem.addEventListener('click', e => {
@@ -72,8 +72,59 @@
     new WOW().init();
 
     //===== mobile-menu-btn
-    let navbarToggler = document.querySelector(".mobile-menu-btn");
+    const navbarToggler = document.querySelector(".mobile-menu-btn");
     navbarToggler.addEventListener('click', function () {
         navbarToggler.classList.toggle("active");
     });
+
+    //Form name validation
+    const formName = document.getElementById('formNome')
+    const spanFormNome = document.getElementById('spanFormNome')
+    formName.addEventListener('input', () => {
+        if (formName.value.length < 3) {
+            spanFormNome.style.opacity = '1'
+        } else {
+            spanFormNome.removeAttribute('style')
+        }
+    })
+
+    //Form email validation
+    const formEmail = document.getElementById('formEmail');
+    const spanFormEmail = document.getElementById('spanFormEmail')
+
+    //Regex for e-mail validation
+    /**
+        ^ indica o início da string
+        [^\s@]+ -> captura um ou mais caracteres que não sejam espaço em branco ou @
+        @ -> captura o caractere @
+        [^\s@]+ -> captura um ou mais caracteres que não sejam espaço em branco ou @
+        \. -> captura o caractere ponto (.) como um caractere literal
+        [^\s@]{2,} -> captura dois ou mais caracteres que não sejam espaço em branco ou @, após o último ponto
+        $ -> indica o final da string
+     */
+    function validarEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{3,}$/
+        return regex.test(email)
+    }      
+
+    formEmail.addEventListener('input', () => {
+        const email = formEmail.value.trim();
+        if (email !== '' && !validarEmail(email)) {
+            spanFormEmail.style.opacity = '1'
+        } else {
+            spanFormEmail.removeAttribute('style')
+        }
+    })
+
+    //Form message validation
+    const formMessage = document.getElementById('formMessage')
+    const spanFormMessage = document.getElementById('spanFormMessage')
+    formMessage.addEventListener('input', () => {
+        if (formMessage.value.length < 3) {
+            spanFormMessage.style.opacity = '1'
+        } else {
+            spanFormMessage.removeAttribute('style')
+        }
+    })
+
 })();
